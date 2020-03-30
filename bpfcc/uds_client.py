5bf1ed1cbfebc3b9af34e8d30477c2b75b4b006d
yuanqili <yuanqili1018@gmail.com>
2020-03-30 10:14:29 -0700

    updates

--- bpfcc/uds_client.py
+++ bpfcc/uds_client.py
@@ -7,5 +7,5 @@ server_address = f'/tmp/trace-{pid}.uds'
 sock = socket.socket(socket.AF_UNIX, socket.SOCK_STREAM)
 sock.connect(server_address)
 sock.sendall(b'hello, this is client')
-data = sock.recv(1024)
+data = sock.recv(10240)
 print(data)
